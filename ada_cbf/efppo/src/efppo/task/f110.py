0e7df609722037c9e99186a1107d6fcdec55f9ab
zwc662 <zwc662@gmail.com>
2024-08-26 15:18:18 -0400

    changed l function

--- ada_cbf/efppo/src/efppo/task/f110.py
+++ ada_cbf/efppo/src/efppo/task/f110.py
@@ -844,7 +844,7 @@ class F1TenthWayPoint(Task):
         l = 0
 
         # Cost for low steer + Cost for higher steer than 5
-        #l = - steer + np.exp(10 * (steer - 5)) - 1
+        l = - steer + np.exp(10 * (steer - 8)) - 1
         
         # Cost for diff from pursuit controller
         if self.cur_pursuit_action is not None:
