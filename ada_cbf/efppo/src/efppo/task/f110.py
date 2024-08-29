855f96069a640594446471e51639cb409e463e28
zwc662 <zwc662@gmail.com>
2024-08-29 11:32:06 -0400

    added back njit nearest_point_on_trajectory

--- ada_cbf/efppo/src/efppo/task/f110.py
+++ ada_cbf/efppo/src/efppo/task/f110.py
@@ -45,7 +45,7 @@ Planner Helpers
 """
 #@njit(fastmath=False, cache=True)
 @jax.jit
-def nearest_point_on_trajectory(point, trajectory):
+def _jit_nearest_point_on_trajectory_(point, trajectory):
     """
     Return the nearest point along the given piecewise linear trajectory.
 
@@ -77,6 +77,43 @@ def nearest_point_on_trajectory(point, trajectory):
     dists = jax.vmap(lambda proj_p: jnp.sqrt(np.sum(jnp.square(point - proj_p))))(projections)
     min_dist_segment = jnp.argmin(dists)
     return projections[min_dist_segment], dists[min_dist_segment], t[min_dist_segment], min_dist_segment
+
+
+@njit(fastmath=False, cache=True)
+def nearest_point_on_trajectory(point, trajectory):
+    """
+    Return the nearest point along the given piecewise linear trajectory.
+
+    Same as nearest_point_on_line_segment, but vectorized. This method is quite fast, time constraints should
+    not be an issue so long as trajectories are not insanely long.
+
+        Order of magnitude: trajectory length: 1000 --> 0.0002 second computation (5000fps)
+
+    point: size 2 numpy array
+    trajectory: Nx2 matrix of (x,y) trajectory waypoints
+        - these must be unique. If they are not unique, a divide by 0 error will destroy the world
+    """
+    diffs = trajectory[1:,:] - trajectory[:-1,:]
+    diffs = np.concatenate((diffs, (trajectory[0] - trajectory[-1]).reshape(1, -1)), axis = 0)
+    l2s   = np.sqrt(np.square(diffs).sum(axis = -1))
+    # this is equivalent to the elementwise dot product
+    # dots = np.sum((point - trajectory[:-1,:]) * diffs[:,:], axis=1)
+    dots = np.empty((trajectory.shape[0], ))
+    for i in range(dots.shape[0]):
+        dots[i] = np.dot((point - trajectory[i, :]), diffs[i, :])
+    t = dots / l2s
+    t[t<0.0] = 0.0
+    t[t>1.0] = 1.0
+    # t = np.clip(dots / l2s, 0.0, 1.0)
+    projections = trajectory + (t*diffs.T).T
+    # dists = np.linalg.norm(point - projections, axis=1)
+    dists = np.empty((projections.shape[0],))
+    for i in range(dists.shape[0]):
+        temp = point - projections[i]
+        dists[i] = np.sqrt(np.sum(temp*temp))
+    min_dist_segment = np.argmin(dists)
+    return projections[min_dist_segment], dists[min_dist_segment], t[min_dist_segment], min_dist_segment
+
 
 
 #@njit(fastmath=False, cache=True)
