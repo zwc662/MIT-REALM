8582d3309ecbea1211be0f05a9eeec3c0ed58ae2
zwc662 <zwc662@gmail.com>
2024-09-18 03:20:12 -0400

    agent control for evaluation

--- ada_cbf/efppo/src/efppo/rl/collector.py
+++ ada_cbf/efppo/src/efppo/rl/collector.py
@@ -159,7 +159,7 @@ def collect_single_env_mode(
         #control = a_pol.mode()
         agent_control = get_pol(obs_pol, state.z)
         expert_control = task.get_expert(state.state, agent_control)
-        envstate_new, control = task.step(state.state, agent_control)
+        envstate_new, control = task.step(state.state, agent_control, control_mode = None)
  
         # Z dynamics.
         l = task.l(envstate_new, control)
